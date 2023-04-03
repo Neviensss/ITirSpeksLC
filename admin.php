88ec4f9232f7c4b4e9194338ffc70998854f4dad
Niks <niks.leimanis3@gmail.com>
2023-04-03 16:05:44 +0300

    Pievienota savieniba ar db vakancem

--- admin.php
+++ admin.php
@@ -2,8 +2,48 @@
     session_start();
     if(isset($_SESSION['lietotajvards'])){
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vakances</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/167/167515.png" type="image/x-icon">
+</head>
 
+<section class="admin">
+    <div class="row">
+        <div class="info">
+            <div class="head-info head-color">Vakanču administrēšana<span><form action='specialitateAdd.php' method='POST'>
+                                <button type='submit' name='apskatitSpec' class='btn2 specbtn'>Pievienot vakanci <i class='fas fa-circle-plus'></i></button>
+                            </form></span></div>
+            <table class="adminTabula">
+                <tr>
+                    <th>Attēls</th>
+                    <th>Vakance</th>
+                    <th>Apraksts</th>
+                    <th></th>
+                    <th></th>
+            </tr>
+            <?php
+                require("faili/connect_db.php");
+                $atlasit_spec_SQL = "SELECT * FROM vakances";
 
+                $atlasaSpec = mysqli_query($savienojums, $atlasit_spec_SQL);
+
+                while($ieraksts = mysqli_fetch_assoc($atlasaSpec)){
+                    echo "
+                    <tr>
+                        <td class='specname'>{$ieraksts['Nosaukums']}</td>
+                        <td class='specdesc'>{$ieraksts['Apraksts']}</td>
+                    </tr>
+                    ";
+                }
+            ?>
+            </table>
+        </div>
+    </div>
+</section>
 
 <?php
 }else{
